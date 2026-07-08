45f7467c25f6bc9efc890039d0bd30f0b84278d7
Mr Ameya Hattarke <ameyahattarke@gmail.com>
2026-07-08 09:13:15 +0530

    switchcase and assignment operator conditional statement

---
+++ js/day 3 conditional statement and assignment operator/script.js
@@ -0,0 +1,117 @@
+let a = 56, b = 20;
+
+console.log(a += b);
+console.log(a *= b);
+console.log(a /= b);
+console.log(a %= b);
+console.log(a **= b);
+console.log(a -= b);
+
+
+// increament decrement 
+
+let c = 25;
+
+console.log("Value of c before post increment: ", c);
+console.log("While Post increment : ", c++);
+console.log("Value of c after post increment: ", c);
+
+console.log("Value of c before pre increment: ", c);
+console.log("While pre increment", ++c);
+console.log("Value of c after pre increment: ", c);
+
+//tirnary operators
+
+let x = 220, y = 55;
+let result;
+result = (x > y) ? "The x is greater then y" : "The y is greater then x"
+console.log(result);
+
+//unary + 
+
+a = 5, b = "5";
+
+console.log(a - b);
+console.log(a + (-b));
+
+//COnditional statements
+
+let q = 55, r = 44, d = 22;
+if (q > r) {
+    console.log("If block executed")
+}
+else if (r > d) {
+    console.log("else if block executed")
+
+}
+else if (r > d) {
+    console.log("else if block executed")
+
+}
+else if (r > d) {
+    console.log("else if block executed")
+
+}
+else if (r > d) {
+    console.log("else if block executed")
+
+}
+else if (r > d) {
+    console.log("else if block executed")
+
+}
+else if (r > d) {
+    console.log("else if block executed")
+
+}
+else if (r > d) {
+    console.log("else if block executed")
+
+}
+else if (r > d) {
+    console.log("else if block executed")
+
+}
+else {
+    console.log("Else block executed");
+}
+
+q = 25, r = 34, d = 54;
+
+if (q > r) {
+    if (q > d) {
+        console.log("The q is greater in all")
+    }
+    else {
+        console.log("The d is greater in all")
+
+    }
+}
+else {
+    if (r > d) {
+        console.log("The r is greater in all")
+    }
+    else {
+        console.log("The d is greater in all")
+    }
+}
+
+let choice = prompt("What is the time now - evening/morning/afternoon ");
+console.log(choice);
+switch (choice) {
+    case 'evening':
+        console.log("Good evening");
+        break;
+    case "morning":
+        console.log("Good morning");
+        break;
+    case "afternoon":
+        console.log("Good afternoon");
+        break;
+    default:
+        console.log("Wrong selection . . . . ");
+        break;
+}
+
+//By using switch case create a calculator take input from user for two variable and ask user wht operation he want to perform on it.
+
