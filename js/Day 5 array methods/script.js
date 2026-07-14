8d5fbc55216a632ffbb962dfab9b796b215eda90
Mr Ameya Hattarke <ameyahattarke@gmail.com>
2026-07-14 09:26:49 +0530

    Array methods complete.

--- js/Day 5 array methods/script.js
+++ js/Day 5 array methods/script.js
@@ -47,3 +47,115 @@ even_ele.forEach(
 );
 
 console.log(`THis is before filter ${ab} ${ab.length} after filteration ${even_ele}`);
+// Every method
+let even = [12, 34, 56, 22, , 88, 44, 888];
+
+let result = even.every((item) => {
+
+    return item % 2 === 0;
+});
+
+console.log(`The Result of even checking is : ${result}`);
+
+let height = [188, 185, 186, 185, 188, 185, 188];
+
+result = height.every((item) => {
+    return item > 183;
+})
+console.log("Is your team above height 183 cm : " + result);
+
+// Find method 
+
+let pq = [57, 2341, 13, 367, 53, 200];
+
+let found_number = pq.find((item) => {
+
+    return item % 2 === 0
+
+});
+
+ab.forEach((item) => {
+    console.log(`This is array item : ${item}`)
+})
+
+console.log(`From above this number ${found_number} is the first element who satifies the condition..`);
+
+//reduce 
+
+let qr = [20, 35, 55, 68, 98];
+
+result = qr.reduce((pre, curr) => {
+    return pre + curr;
+})
+
+console.log(result);
+//push method
+console.log("Current array " + qr);
+
+qr.push(5555);
+
+console.log("After push array " + qr);
+
+//pop method
+
+let poppedelement = qr.pop();
+
+console.log(`Popped element is ${poppedelement}  and now the array is ${qr}`)
+
+poppedelement = qr.pop();
+
+console.log(`Popped element is ${poppedelement}  and now the array is ${qr}`)
+poppedelement = qr.pop();
+
+console.log(`Popped element is ${poppedelement}  and now the array is ${qr}`)
+poppedelement = qr.pop();
+
+console.log(`Popped element is ${poppedelement}  and now the array is ${qr}`)
+
+//shift it removes element from the begining
+qr = [25, 2, 34, 54, 45, 3, 434, 23, 23, 89];
+let shifted_element = qr.shift();
+console.log(`Shifted element is ${shifted_element}  and now the array is ${qr}`)
+
+shifted_element = qr.shift();
+console.log(`Shifted element is ${shifted_element}  and now the array is ${qr}`)
+
+shifted_element = qr.shift();
+console.log(`Shifted element is ${shifted_element}  and now the array is ${qr}`)
+
+shifted_element = qr.shift();
+console.log(`Shifted element is ${shifted_element}  and now the array is ${qr}`)
+
+//unshift array
+
+qr.unshift(45);
+qr.unshift(22222);
+qr.unshift(2111);
+qr.unshift(99992);
+console.log(`array after unshifting elements ${qr}`)
+
+//splice with one parameter
+
+let mr = [8, 37, 48, 37, 2, 834, 2, 398, 47, 293, 47];
+
+console.log(mr);
+mr.splice(2);
+
+console.log(mr);
+
+//splice with 2 prameter
+mr = [8, 37, 48, 37, 2, 834, 2, 398, 47, 293, 47];
+
+console.log(mr);
+mr.splice(2, 5);
+
+console.log(mr);
+
+//splice with 3 parameters 
+
+mr = [8, 37, 48, 37, 2, 834, 2, 398, 47, 293, 47];
+
+console.log(mr);
+mr.splice(2, 5, 999, 8888, 7777, 6666, 555);
+console.log(mr);
+
